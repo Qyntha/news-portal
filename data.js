62ad23a475777399f670e102571cde7a2f9a79d9
Xihe <xihe1232021@163.com>
2026-08-31 19:16:27 +0800

    fix: 将便民服务政务平台链接更换为济宁市政府官网

--- data.js
+++ data.js
@@ -23,7 +23,7 @@ const services = [
   { name: '天气', link: 'https://www.weather.com.cn', icon: 'M3 12a9 9 0 1118 0M3 12h18M12 3a15 15 0 010 18M7.7 7.7a10 10 0 010 8.6M16.3 7.7a10 10 0 010 8.6' },
   { name: '快递查询', link: 'https://www.kuaidi100.com', icon: 'M20 8l-4-3H4a6 6 0 016 6h10zM20 8v8a2 2 0 01-2 2H6a4 4 0 01-4-4v-3m0 5.5L6 21m3-3.5L9 21M2 13h18' },
   { name: '招聘信息', link: 'https://www.zhipin.com', icon: 'M20 7a2 2 0 00-2-2h-3l1-2h-6l1 2H6a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V7zM8 12h8M8 16h5' },
-  { name: '政务平台', link: 'https://www.gov.cn', icon: 'M4 21V9l8-5 8 5v12M4 21h16M9 21v-6h6v6M12 11v2' },
+  { name: '济宁市政府', link: 'https://www.jining.gov.cn', icon: 'M4 21V9l8-5 8 5v12M4 21h16M9 21v-6h6v6M12 11v2' },
   { name: '交通出行', link: 'https://www.12306.cn', icon: 'M4 6a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 01-2 2H6a2 2 0 01-2-2V6zm0 6h12m-5 5a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2z' },
   { name: '医疗预约', link: 'https://www.guahao.com', icon: 'M12 3v18M3 12h18M8 4l4 4 4-4M8 20l4-4 4 4' },
   { name: '教育考试', link: 'https://www.neea.edu.cn', icon: 'M12 3L2 8l10 5 10-5-10-5zM4 10v6m0 0a8 5 0 0016 0M6 12.5V17a6 3.5 0 0012 0v-4.5' },
